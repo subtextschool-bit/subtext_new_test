209434dc7fa28b0711239ecdbd7da01f40d202cc
polinareutova5-web <polinareutova5@gmail.com>
2026-05-31 22:44:42 +0300

    Update subtext.js

--- subtext.js
+++ subtext.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwptA-FiKIHws7T1BiBKqwqFti7Rz-8l-6Bq-KGh-rP42wbD_hf6Z2uEE8bi5e7pHIs/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzaSUUSenE5ovI2ZzzJN99ItHtyvHdQO4CImyPDdScKO5mjNMJof3Qliw87HIRdxIIW/exec";
 let userId = "";
 let username = "";
 let currentCourse = "";
